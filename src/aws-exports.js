0b92868193cf8b48b13256cc9ff4888231ecc9de
Ethan Partain <73601509+ethanpartain@users.noreply.github.com>
2025-10-29 19:05:04 -0700

    Create aws-exports.js

---
+++ src/aws-exports.js
@@ -0,0 +1,13 @@
+const awsmobile = {
+  aws_project_region: 'us-east-2',
+  aws_cognito_region: 'us-east-2',
+  aws_user_pools_id: 'us-east-2_VTgeYopXD',
+  aws_user_pools_web_client_id: '6qarqkj959kth0hdcbpgp79bo2',
+  Auth: {
+    region: 'us-east-2',
+    userPoolId: 'us-east-2_VTgeYopXD',
+    userPoolWebClientId: '6qarqkj959kth0hdcbpgp79bo2',
+  }
+};
+
+export default awsmobile;
