ac78323c7ab0cea4b285c5f2200aa9a04f2e135e
Ethan Partain <73601509+ethanpartain@users.noreply.github.com>
2025-10-29 17:09:23 -0700

    Update main.jsx
    auth

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Authenticator } from '@aws-amplify/ui-react'
 import App from './App.jsx'
 import './index.css'
+import { Authenticator } from '@aws-amplify/ui-react'
 import '@aws-amplify/ui-react/styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Authenticator>
-      <App />
+      {({ signOut, user }) => (
+        <App signOut={signOut} user={user} />
+      )}
     </Authenticator>
   </React.StrictMode>,
 )
