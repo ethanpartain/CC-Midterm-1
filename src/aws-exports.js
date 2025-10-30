6e8582bdb74ff5b086ee715e33c54ed1bad5cc5a
Ethan Partain <73601509+ethanpartain@users.noreply.github.com>
2025-10-29 19:29:29 -0700

    Update aws-exports.js

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -2,11 +2,11 @@ const awsmobile = {
   aws_project_region: 'us-east-2',
   aws_cognito_region: 'us-east-2',
   aws_user_pools_id: 'us-east-2_VTgeYopXD',
-  aws_user_pools_web_client_id: '2t1fno71qmf8jubapk2sg5x7m',
+  aws_user_pools_web_client_id: '2l1fnn71rqmfdjubapk2sg5v7m',
   Auth: {
     region: 'us-east-2',
     userPoolId: 'us-east-2_VTgeYopXD',
-    userPoolWebClientId: '2t1fno71qmf8jubapk2sg5x7m',
+    userPoolWebClientId: '2l1fnn71rqmfdjubapk2sg5v7m',
   }
 };
 
